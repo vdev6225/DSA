8d1f9f92534952a8e7b00b1a931dd478b991c049
Vishnu <106086134+vdev6225@users.noreply.github.com>
2024-10-18 23:52:22 +0530

    commit

--- src/Components/Home/PodcastSection/PodcastSection.jsx
+++ src/Components/Home/PodcastSection/PodcastSection.jsx
@@ -1,8 +1,9 @@
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import NoImg from "../../../assets/img/no-image.jpg";
+import Arrow from "../../../assets/img/icons/Vector.png";
 import "./PodcastSection.css";
-export default function PodcastSection({banners}) {
+export default function PodcastSection({ banners }) {
     return (
         <section className="section-spacing px-0 podcast-section">
             <div className="container-fluid">
@@ -13,12 +14,13 @@ export default function PodcastSection({banners}) {
                             <div className="content">
                                 <div className="text">
                                     <h2 className="main-heading">
-                                        podcasts <FaArrowRight /><br />
+                                        podcasts <br />
                                         that will inspire <br /> you to grow
                                     </h2>
                                     <p>
                                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
                                     </p>
+                                    <img src={Arrow} className="img-fluid" alt="" />
                                 </div>
                                 <div className="youtube-btn">
                                     <Link to="">youtube channel</Link>
@@ -28,21 +30,27 @@ export default function PodcastSection({banners}) {
                         <div className="bottom-two-cards">
                             <div className="b-card">
                                 <img src={NoImg} className="img-fluid" alt="" />
-                                <span>on June 8, 2020</span>
-                                <p>#Loremipsum</p>
+                                <div>
+                                    <span>on June 8, 2020</span>
+                                    <p>#Loremipsum</p>
+                                </div>
                             </div>
                             <div className="b-card">
                                 <img src={NoImg} className="img-fluid" alt="" />
-                                <span>on June 8, 2020</span>
-                                <p>#Loremipsum</p>
+                                <div>
+                                    <span>on June 8, 2020</span>
+                                    <p>#Loremipsum</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-5">
                         <div className="right-card">
                             <img src={banners[0]?.fld_image} alt="" className="img-fluid" />
-                            <span>{banners[0]?.fld_created_by}</span>
-                            <p>{banners[0]?.fld_short_desc}</p>
+                            <div>
+                                <span>{banners[0]?.fld_created_by}</span>
+                                <p>{banners[0]?.fld_short_desc}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
